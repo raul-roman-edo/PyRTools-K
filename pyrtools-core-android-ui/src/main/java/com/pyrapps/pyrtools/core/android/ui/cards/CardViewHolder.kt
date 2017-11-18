79beb22df68aa00248a95852c4fe8e6d95deb0b0
rroman <r.romanlopez@gmail.com>
2017-11-18 13:37:24 +0100

    [ADD] constructor variant and update libraries

--- pyrtools-core-android-ui/src/main/java/com/pyrapps/pyrtools/core/android/ui/cards/CardViewHolder.kt
+++ pyrtools-core-android-ui/src/main/java/com/pyrapps/pyrtools/core/android/ui/cards/CardViewHolder.kt
@@ -1,11 +1,15 @@
 package com.pyrapps.pyrtools.core.android.ui.cards
 
 import android.support.v7.widget.RecyclerView.ViewHolder
+import android.view.View
 import android.view.ViewGroup
 import com.pyrapps.pyrtools.core.android.ui.extensions.inflateView
 
 
-class CardViewHolder(parent: ViewGroup, layoutId: Int) : ViewHolder(parent.inflateView(layoutId)) {
+class CardViewHolder(view: View?) : ViewHolder(view) {
+
+    constructor(parent: ViewGroup, layoutId: Int) : this(parent.inflateView(layoutId))
+
     fun fillWith(card: Card<*>) {
         itemView.tag = card
     }
